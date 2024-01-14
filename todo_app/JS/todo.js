0774280965e1305e766b377fca4365841a174de3
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-01-13 21:10:40 -0500

    Update todo.js

--- todo_app/JS/todo.js
+++ todo_app/JS/todo.js
@@ -44,3 +44,19 @@ function show(){
 document.getElementById('add').addEventListener('click', add);
 /*This will keep the inputs displayed permantaly on the screen */
 show();
+/*This creates the functionality of removing a todo item from the array*/
+function remove(){
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    /*This looks in the show() how to display a removed item on the screen */
+    show();
+    return false;
+}
+/*This tells the browser how to display the todo
+array after an item has been removed */
+var buttons = document.getElementsByClassName('remove');
+for (var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', remove);
+};
